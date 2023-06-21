c7484b0db45f8c0d3c02c4b56e150f062e1251e0
Andrey <it.personage@gmail.com>
2023-06-20 18:36:20 -0600

    lesson 6 AuthHOC

---
+++ src/Components/HOC/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const AuthHOC: React.FC = ({ children }) => {
+  const isAuth = false;
+
+  if (isAuth) {
+    return <>{children}</>;
+  }
+  return <Navigate to="/auth" />;
+};
+
+export default AuthHOC;
